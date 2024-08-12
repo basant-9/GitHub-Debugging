5fb2196876c7465d3bdf73cd7ee48667ccb5c2a4
bassant <basantihab99@gmail.com>
2024-08-13 02:45:42 +0300

    done late

--- Buggggggs.py
+++ Buggggggs.py
@@ -1,10 +1,10 @@
 # Fibbonacci sequence calculation
 a, b, n = 0, 1, 10
 fibonacci = []
-for i in range(n) 
+for i in range(n) :
     fibonacci.append(a)
     a, b = b, a + b
-pritn(f"Fibbonacci sequence of {n} numbers: {fibonacci}")  
+print(f"Fibbonacci sequence of {n} numbers: {fibonacci}")  
 
 # Find minimum and maximum in a list
 numbers = [3, 5, 1, 10, 2, 7, 6, 4, 8, 9]
@@ -12,10 +12,10 @@ min_value = max_value = numbers[0]
 for number in numbers:
     if number < min_value:
         min_value = number
-    elif number != max_value:
+    elif number > max_value:
         max_value = number
-print(f"Minimum value: {max_value}")
-print(f"Maximum value: {min_value}")
+print(f"Minimum value: {min_value}")
+print(f"Maximum value: {max_value}")
 
 # Basic arithmetic calculations
 x = 10
@@ -23,10 +23,11 @@ y = 0
 sum = x + y
 difference = x - y
 product = x * y
-quotient = x / y
-
+if y != 0:
+    quotient = x / y
+else:
+    quotient = 'error' 
 print(f"Sum: {sum}, Difference: {difference}, Product: {product}, Quotient: {quotient}")
-
 # Prime number check
 num = 29
 is_prime = True
@@ -37,20 +38,21 @@ for i in range(2, num//2):
 if is_prime:
     print(f"{num} is a prime number")
 else:
-    print(f"{num} is not a prime number")
+    print(f"{num} is not a prime number") 
 
+# string check 
 original_string = "Hello, World!"
 reversed_string = ""
-for i in range(len(original_string), 0, -1):
+for i in range(len(original_string), 0, +1):
     reversed_string += original_string[i]
 print("Original string:", original_string)
-print("Reversed string:", reversed_string)
+print("Reversed string:", reversed_string) 
 
 # Sum of squares of first n natural numbers
 n = 5
 sum_of_squares = 0
 for i in range(n): 
-    sum_of_squares += i^2  
+    sum_of_squares += i*2  
 print(f"Sum of squares of first {n} natural numbers: {sum_of_squares}")
 
 # Count the number of vowels in a string
@@ -58,7 +60,7 @@ string = "Protons is Amazing"
 vowels = "aeiou"
 vowel_count = 0
 for char in string:
-    if char in vowel:
+    if char in vowels:
         vowel_count += 1
 print(f"Number of vowels in the string: {vowel_count}")
 
@@ -77,9 +79,9 @@ else:
 # Sum of positive elements in a list
 numbers = [1, 2, -9, -1 , 3, 4, -7, 5]
 sum_elements = 0
-for num in number: 
+for num in numbers: 
     if not num > 0:
-    sum_elements += num
+        sum_elements += num
 print(f"Sum of elements: {sum_elements}")
 
 # Factorial calculation
@@ -96,35 +98,34 @@ for i in range(1, 11):
 
 # Checking if a number is even or odd
 number = 15
-if number%2 = 0:  
+if number%2 == 0:  
     print(f"{number} is even")
-else
+else:
     print(f"{number} is odd") 
-
 
 # Appending four elements to the end of the list
 my_list = [1, 2, 3]
-my_list.append(4, 5)  
-my_list.append([4, 5]) 
-print("My list = " + my_list)
+my_list.append (4)
+my_list.append (5)
+my_list.append (4)
+my_list.append (5)
+print(my_list)
 
 # Comparing different types
 value1 = 10
-value2 = "10"
+value2 = 10
 if value1 == value2:
     print("Values are equal")
 else:
     print("Values are not equal")
 
-
-
 # Count even numbers from Zero to 50
 count = 0
 while True:
-    if count == 51:
+    if count == 50:
         break
-    count += 2 
-
+    count += 2
+    print (count)
 
 # Sum of first n natural numbers
 n = 10
@@ -152,23 +153,20 @@ print(f"Sum of digits: {sum_of_digits}")
 
 # Check if a number is a perfect square
 num = 25
-if int(num  0.5) * int(num  0.5) = num:  
+if int(num ** 0.5) * int(num ** 0.5) == num:  
     print(f"{num} is a perfect square")
 else:
     print(f"{num} is not a perfect square")
-
 
 # Remove duplicates from a list
 numbers = [1, 2, 2, 3, 4, 4, 5, 5 , 5]
 unique_numbers = []
 for number in numbers:
-    if number in unique_numbers:
-        unique_numbers.add(number)  # AttributeError: 'list' object has no attribute 'add'
+    if number not in unique_numbers:
+        unique_numbers.append(number)  # AttributeError: 'list' object has no attribute 'add'
 print(f"Unique numbers: {unique_numbers}")
 
 # Swap two variables
 a = 5
 b = 10
-a = b
-b = a
-print(f"After swapping: a = {a}, b = {b}")
+print(f"After swapping: a = {b}, b = {a}") 
